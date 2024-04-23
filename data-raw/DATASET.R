c5b14f8f015a7a9c7b23735802bdb0865f50917b
Jared Hileman <156353004+Jaredhileman@users.noreply.github.com>
2024-04-23 10:18:51 -0600

    updated dataset file with Sai's changes, removed old templates

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,65 +2,112 @@ ID = "b6bed725df9a4966a23b8984a2d40457"
 secret = "5609c10a964b4f2082f47842a5d2fc50"
 
 access_token <- spotifyr::get_spotify_access_token(ID, secret)
-authorization <- spotifyr::get_spotify_authorization_code(ID,
-                                                            secret,
-                                                            "user-top-read user-library-read")
+authorization <- spotifyr::get_spotify_authorization_code(ID, secret,
+                                                          scope = c("user-top-read", "user-read-recently-played",
+                                                                    "user-read-currently-playing"))
 
-# Obtain top 1000 artists
+
+
+# Obtain top medium_term artists
 offset = seq(0, 950, 50)
 top_artists_longterm <- data.frame()
 for (batch in offset) {
   curr_artists <- spotifyr::get_my_top_artists_or_tracks(type = "artists",
-                                                        limit = 50,
-                                                        offset = batch,
-                                                        time_range = "long_term",
-                                                        authorization = authorization)
+                                                         limit = 50,
+                                                         offset = batch,
+                                                         time_range = "medium_term",
+                                                         authorization = authorization)
   top_artists_longterm <- dplyr::bind_rows(top_artists_longterm, curr_artists)
 }
 
 
-# Obtain top 1000 tracks
+# Obtain top medium_term tracks
 top_tracks_longterm <- data.frame()
 for (batch in offset) {
   curr_tracks <- spotifyr::get_my_top_artists_or_tracks(type = "tracks",
-                                                         limit = 50,
-                                                         offset = batch,
-                                                         time_range = "long_term",
-                                                         authorization = authorization)
+                                                        limit = 50,
+                                                        offset = batch,
+                                                        time_range = "medium_term",
+                                                        authorization = authorization)
   top_tracks_longterm <- dplyr::bind_rows(top_tracks_longterm, curr_tracks)
 }
 
-
-
-
-
+#offset value
 offset_length <- ceiling(spotifyr::get_my_saved_tracks(include_meta_info = TRUE,
                                                        authorization = authorization)[['total']] / 50)
 offset_total <- (seq(offset_length) - 1) * 50
 
-
-# Obtain saved tracks
+## Obtain saved tracks
 saved_tracks<- data.frame()
 for (batch in offset_total) {
   curr_saved_tracks <- spotifyr::get_my_saved_tracks(limit = 50,
-                                                        offset = batch,
-                                                        authorization = authorization)
+                                                      offset = batch,                                                      authorization = authorization)
   saved_tracks <- dplyr::bind_rows(saved_tracks, curr_saved_tracks)
 }
 
-# Get artist name
-for (i in 1:nrow(saved_tracks)) {
-  saved_tracks$artist[i] <- saved_tracks$track.artists[i][[1]]$name
-  audio_features <- spotifyr::get_track_audio_features(saved_tracks$track.id[i],
-                                                       access_token = access_token)
-  saved_tracks$energy[i] <- audio_features$energy
-  saved_tracks$valence[i] <- audio_features$valence
-}
-
-
-# q: how to append data frames to the end of data frames in R
-# a: https://stackoverflow.com/questions/3505701/rbind-data-frames-in-a-list
 
 usethis::use_data(saved_tracks, overwrite = TRUE)
 usethis::use_data(top_artists_longterm, overwrite = TRUE)
 usethis::use_data(top_tracks_longterm, overwrite = TRUE)
+
+###################################
+###################################
+
+# Fetch data using the calculated date range - MEDIUM TERM (approx 6 months)
+offset = seq(0, 950, 50)
+top_artists_mediumterm <- data.frame()
+for (batch in offset) {
+  curr_artists <- spotifyr::get_my_top_artists_or_tracks(type = "artists",
+                                                         limit = 50,
+                                                         offset = batch,
+                                                         time_range = "medium_term",
+                                                         authorization = authorization)
+  top_artists_mediumterm <- dplyr::bind_rows(top_artists_mediumterm, curr_artists)
+}
+
+
+# Obtain top medium_term tracks
+top_tracks_mediumterm <- data.frame()
+for (batch in offset) {
+  curr_tracks <- spotifyr::get_my_top_artists_or_tracks(type = "tracks",
+                                                        limit = 50,
+                                                        offset = batch,
+                                                        time_range = "medium_term",
+                                                        authorization = authorization)
+  top_tracks_mediumterm <- dplyr::bind_rows(top_tracks_mediumterm, curr_tracks)
+}
+
+# usethis::use_data(saved_tracks, overwrite = TRUE)
+usethis::use_data(top_artists_mediumterm, overwrite = TRUE)
+usethis::use_data(top_tracks_mediumterm, overwrite = TRUE)
+
+
+###################################
+
+# Fetch data using the calculated date range - SHORT TERM (approx 1 month)
+offset = seq(0, 950, 50)
+top_artists_shortterm <- data.frame()
+for (batch in offset) {
+  curr_artists <- spotifyr::get_my_top_artists_or_tracks(type = "artists",
+                                                         limit = 50,
+                                                         offset = batch,
+                                                         time_range = "short_term",
+                                                         authorization = authorization)
+  top_artists_shortterm <- dplyr::bind_rows(top_artists_shortterm, curr_artists)
+}
+
+
+# Obtain top medium_term tracks
+top_tracks_shortterm <- data.frame()
+for (batch in offset) {
+  curr_tracks <- spotifyr::get_my_top_artists_or_tracks(type = "tracks",
+                                                        limit = 50,
+                                                        offset = batch,
+                                                        time_range = "short_term",
+                                                        authorization = authorization)
+  top_tracks_shortterm <- dplyr::bind_rows(top_tracks_shortterm, curr_tracks)
+}
+
+# usethis::use_data(saved_tracks, overwrite = TRUE)
+usethis::use_data(top_artists_shortterm, overwrite = TRUE)
+usethis::use_data(top_tracks_shortterm, overwrite = TRUE)
