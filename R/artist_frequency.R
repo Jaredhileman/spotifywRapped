9e28357ef878024c5240d4132485ea217ffe9bb9
lrose21 <a02357321@usu.edu>
2024-04-23 18:28:02 -0600

    Updated Function 2

--- R/artist_frequency.R
+++ R/artist_frequency.R
@@ -38,16 +38,42 @@ top_tracks <- rbind(top_tracks,
                                                  authorization = authorization))
 
 # graphing
-occurences <- table(unlist(top_tracks$album.name))
+occurences <- table(unlist(saved_tracks$artist))
 df <- as.data.frame(occurences)
 data_sorted <- df[order(df$Freq,
-                                decreasing = TRUE), ]
+                        decreasing = TRUE), ]
 new_df = head(data_sorted, 10)
-graph <- ggplot(data = new_df, aes(x = Var1, y = Freq)) +
-  geom_bar(stat = "identity", fill = "steelblue") +
-  geom_text(aes(label = Var1), vjust=1.6, color="orange", size = 3) +
-  scale_x_discrete(labels = NULL, breaks = NULL) +
-  labs(x = "", y = "") +
-  #scale_color_manual(palette = c()) +
-  theme_minimal()
 
+
+#our data
+soft_p = c('#66545e', '#eda990', '#aa6f73', '#a39193', '#f6e0b5')
+y_max <- ceiling(max(new_df$Freq) / 5) * 5
+graph <- ggplot(data = new_df,
+                aes(x = reorder(Var1, -Freq), y = Freq)) +
+  geom_bar(stat = "identity", fill = soft_p[3]) +
+  geom_text(aes(label = Var1),
+            vjust = -1,
+            hjust = 0,
+            color = soft_p[3],
+            size = 3,
+            angle = 30,
+            position = position_nudge(x = -0.25)) +
+  scale_x_discrete(labels = NULL, breaks = NULL) +
+  scale_y_continuous(breaks = seq(0, y_max, 5), limits = c(0, y_max)) +
+  labs(x = "", y = "") +
+  scale_color_manual(values = soft_p) +
+  #scale_color_manual(palette = c()) +
+  theme_minimal() +
+  theme(axis.line = element_line(color = soft_p[1]),
+        axis.line.x = element_blank(),
+        axis.text = element_text(color = soft_p[1]),
+        panel.grid.major = element_line(color = soft_p[1],
+                                        linetype = "dotted"),
+        panel.grid.minor = element_line(color = soft_p[1],
+                                        linetype = "dotted")) +
+  coord_cartesian(ylim = c(0, max(new_df$Freq) +
+                             5 * length(new_df$Var1[1])),
+                  xlim = c(1, nrow(new_df) +
+                             length(new_df$Var1[length(new_df$Var1)])))
+
+graph
